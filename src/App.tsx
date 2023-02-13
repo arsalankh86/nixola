ab05f8fc1d03508b5de4bc44b19c9be6bad58590
arsalan.khan <arsalan.khan@seekinvest.com>
2023-02-13 19:30:56 +0500

    Project Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { getAllPokemonList } from "../api/dataApi";
 
 function App() {
+  const [pokemonData,setPokemonData] = useState([]);
+  
+  useEffect(() => {
+    async function fetchData(){
+      const data = await getAllPokemonList();
+      setPokemonData(data?.results);
+    }
+    fetchData();
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{marginTop:'40px',justifyContent:'space-around',display:'flex',flexWrap:'wrap', width:'90%', margin:'auto'}}>
+      
+      {pokemonData?.map((poke:any,i:any) => {
+        return (
+          <div style={{width: '400px',height:'330px', border:'2px solid #000000', margin:'30px 10px'}}>
+              <div style={{padding:'5px 10px'}}>
+                <p style={{fontWeight:'bold',textTransform:'capitalize'}}> {poke?.name}</p>
+              </div>
+              <img style={{height: '300px', width:'300px'}} alt="pokemon" src={`https://img.pokemondb.net/artwork/large/${poke?.name}.jpg`}/>
+          </div>
+        )
+      })}
+      <div>
+
+      </div>
     </div>
   );
 }
